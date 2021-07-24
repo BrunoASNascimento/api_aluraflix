d92e9f36bde77818436227fcf02e5dc99a2aea9c
Bruno Nascimento <bruno-asn@outlook.com.br>
2021-07-24 13:35:58 -0300

    Reset project

--- test/mongo_write.py
+++ test/mongo_write.py
@@ -6,12 +6,23 @@ from dotenv import load_dotenv, find_dotenv
 from pymongo import cursor
 load_dotenv(find_dotenv())
 
+
 # Connect mongo
 client = pymongo.MongoClient(os.environ.get('MONGO_URL'))
 db = client['study']
 
+
+def get_next_sequence_value(id_value):
+    sequence_document = db.counters.find_one_and_update(
+        {'_id': id_value},
+        {'$inc': {'sequence_value': 1}}
+    )
+    return sequence_document['sequence_value']
+
+
 # Insert a document
-db.develop.insert_one({'test': 'test'})
+db.develop.insert_one(
+    {'test': 'test', "_id": get_next_sequence_value('develop')})
 
 # Get all documents
 cursor = db.develop.find({})
