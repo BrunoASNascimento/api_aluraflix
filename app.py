2a466f9c9767d242de67a04b18e95a8b804f092d
Bruno Nascimento <bruno-asn@outlook.com.br>
2021-07-24 17:48:52 -0300

    Delete test ok

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-import json
 import flask
-from flask import request, jsonify
+from flask import request, jsonify, send_file
 from dotenv import find_dotenv, load_dotenv
 
 from utils_mongodb.mongo_connection import get_db_handle_mongodb
 from utils_mongodb.read_documents import get_all_documents, get_one_document
 from utils_mongodb.create_document import create_document
+from utils_mongodb.delete_document import delete_one_document
 
 app = flask.Flask(__name__)
 
@@ -24,11 +24,13 @@ def list_all_videos():
 
 @app.route('/videos/<int:id>', methods=['GET'])
 def get_one_video(id):
-
     app.logger.info(id)
     db_handle, _ = get_db_handle_mongodb(database_name='study')
     data = get_one_document(db_handle, id)
-    return jsonify(data)
+    if data != None:
+        return jsonify(data)
+    else:
+        return "Não encontrado."
 
 
 @app.route('/videos', methods=['POST'])
@@ -44,9 +46,36 @@ def create_video():
         return jsonify(error)
 
 
+@app.route('/videos/<int:id>', methods=['DELETE'])
+def delete_one_video(id):
+    app.logger.info(id)
+    db_handle, _ = get_db_handle_mongodb(database_name='study')
+    data = delete_one_document(db_handle, id)
+    if data == 0:
+        return jsonify({'message': 'Videos não encontrado', 'error': True})
+    else:
+        return jsonify({'message': f'Video {id} apagado com sucesso', 'error': False})
+
+
+@app.route('/videos', methods=['PUT', 'PATCH'])
+def update_one_video():
+    data_request = request.get_json()
+    db_handle, _ = get_db_handle_mongodb(database_name='study')
+
+    if data_request.get('id'):
+        return jsonify({'message': 'Não foi encontrado o campo "id", por favor, inserir.', 'error': True})
+    document = get_one_document(db_handle, data_request['id'])
+
+
 @app.errorhandler(404)
 def page_not_found(e):
-    return "<h1>404</h1><p>The resource could not be found.</p>", 404
+    url_link = 'https://http.cat/404.png'
+    return (f"""
+    <body style="background-color:black;">
+    <center>
+    <img src="{url_link}">
+    </center>
+    </body>""", 404)
 
 
 if __name__ == "__main__":
